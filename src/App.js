46941e9afb087896648ca1cf0de13523cb1c5216
joelraymann <joelraymann@gmail.com>
2020-05-10 21:18:16 +0530

    created project and added basic structure

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+// import components
+import HomePage from "./homepage.component";
+
+function App() {
+  return (
+    <div>
+      <HomePage></HomePage>
+    </div>
+  );
+}
+
+export default App;
